db6d92a8a3d369127820673db8a12014c7d60981
LiMiaoJing <466020917@qq.com>
2017-05-31 12:44:25 +0800

    Get the maximum, the minimum, the number of elements, the average element

--- main/main.js
+++ main/main.js
@@ -1,18 +1,43 @@
+"use strict"
+
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
+o) 最大值 = ${sequence.max()}
+o) 元素数量 = ${sequence.count()}
+o) 平均值 = ${sequence.average()}`);
 // Write your code here
-`);
 };
 
 class Sequence {
   constructor(input) {
-    // Write your code here
+  var sequence = new Array(input);
+  this.sequence = input;
   }
 
   minimum() {
-    // Write your code here
-  }
+    return Math.min.apply(null, this.sequence);
+  }//返回数组中最小元素
+
+  max() {
+    return Math.max.apply(null, this.sequence);
+  }//返回数组中最大元素
+
+  average() {
+    var sum = 0;
+    for(var i = 0; i<this.sequence.length; i++) {
+      sum = sum + this.sequence[i];
+    }
+    var avr = sum/this.sequence.length;
+    avr = avr.toFixed(2);
+    return avr;
+  }//返回数组中所有元素平均值
+
+  count() {
+    return this.sequence.length;
+  }//返回数组中元素个数
+
+
 
   // Write your code here
 }
--- run-tests.js
+++ run-tests.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const Jasmine = require('jasmine');
 const jasmine = new Jasmine();
 
